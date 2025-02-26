f08c2119ff822c008ffd743a9d2960c10eaad66b
Berserk-Vl <berkut.is.back@gmail.com>
2025-02-27 01:04:02 +0300

    Add javadoc to City class.

--- src/main/java/ru/sb/weather/City.java
+++ src/main/java/ru/sb/weather/City.java
@@ -1,12 +1,39 @@
 package ru.sb.weather;
 
+/**
+ * Stores information about the city.
+ *
+ * @author <a href="https://github.com/Berserk-Vl/">Berserk-Vl</a>
+ */
 public class City {
+    /**
+     * The city name.
+     */
     private String name;
+    /**
+     * The latitude coordinates.
+     */
     private double latitude;
+    /**
+     * The longitude coordinates.
+     */
     private double longitude;
+    /**
+     * The timestamp in milliseconds indicating when weather data for this city was last updated.
+     *
+     * @see System#currentTimeMillis()
+     */
     private long lastUpdate;
+    /**
+     * The string containing current weather data in this city in JSON format.
+     */
     private String weather;
 
+    /**
+     * Constructs a new city object with a given name.
+     *
+     * @param name the city name
+     */
     City(String name) {
         this.name = name;
         latitude = 0;
@@ -15,38 +42,87 @@ public class City {
         weather = "";
     }
 
+    /**
+     * Returns the city name.
+     *
+     * @return the city name
+     */
     String getName() {
         return name;
     }
 
+    /**
+     * Returns the latitude coordinates of the city.
+     *
+     * @return the latitude coordinates of the city
+     */
     double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Sets the latitude coordinates of the city.
+     *
+     * @param latitude the latitude coordinates of the city
+     */
     void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     * Returns the longitude coordinates of the city.
+     *
+     * @return the longitude coordinates of the city
+     */
     double getLongitude() {
         return longitude;
     }
 
+    /**
+     * Sets the longitude coordinates of the city.
+     *
+     * @param longitude the longitude coordinates of the city
+     */
     void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
+    /**
+     * Returns the timestamp in milliseconds indicating the
+     * last time the weather data for this city was updated.
+     *
+     * @return the timestamp in milliseconds
+     * @see System#currentTimeMillis()
+     */
     long getLastUpdate() {
         return lastUpdate;
     }
 
+    /**
+     * Sets the timestamp in milliseconds indicating the
+     * last time the weather data for this city was updated.
+     *
+     * @param lastUpdate the timestamp in milliseconds
+     * @see System#currentTimeMillis()
+     */
     void setLastUpdate(long lastUpdate) {
         this.lastUpdate = lastUpdate;
     }
 
+    /**
+     * Returns the weather data for this city in JSON format.
+     *
+     * @return the weather data for this city in JSON format
+     */
     String getWeather() {
         return weather;
     }
 
+    /**
+     * Sets the weather data for this city.
+     *
+     * @param weather the weather data for this city
+     */
     void setWeather(String weather) {
         this.weather = weather;
     }
