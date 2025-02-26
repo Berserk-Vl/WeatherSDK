094515051fd1dfc8c6880442ebb79e4566e0dac2
Berserk-Vl <berkut.is.back@gmail.com>
2025-02-27 01:00:22 +0300

    Add javadoc to Mode enum.

--- src/main/java/ru/sb/weather/Mode.java
+++ src/main/java/ru/sb/weather/Mode.java
@@ -1,6 +1,18 @@
 package ru.sb.weather;
 
+/**
+ * Used to set the operating mode of the Weather class.
+ *
+ * @author <a href="https://github.com/Berserk-Vl/">Berserk-Vl</a>
+ */
 public enum Mode {
+    /**
+     * A mode in which data is updated only after a request.
+     */
     ON_DEMAND,
+    /**
+     * A mode in which data is updated automatically after a certain
+     * period of time or upon request.
+     */
     POLLING
 }
